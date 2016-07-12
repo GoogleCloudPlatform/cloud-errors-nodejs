9f559ba91688d6b28f6d5e60a5acc87a17a412ad
Ali Ijaz Sheikh <ofrobots@google.com>
2016-07-12 07:46:21 -0700

    Avoid using Object.assign (#12)
    It is not available on Node.js 0.12. Use lodash.assign instead.

--- tests/unit/testExpressRequestInformationExtractor.js
+++ tests/unit/testExpressRequestInformationExtractor.js
@@ -17,6 +17,7 @@
 var test = require('tape');
 var expressRequestInformationExtractor = require('../../lib/request-extractors/express.js');
 var Fuzzer = require('../../utils/fuzzer.js');
+var _ = require('lodash');
 
 test(
   'Test request information extraction given invalid input'
@@ -118,7 +119,7 @@ test(
 
     var headerFactory = function ( toDeriveFrom ) {
 
-      var lrn = Object.assign({}, toDeriveFrom);
+      var lrn = _.assign({}, toDeriveFrom);
       lrn.header = function ( toRet ) {
 
         if (lrn.hasOwnProperty(toRet)) {
