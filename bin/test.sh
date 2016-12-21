71a29685651a230f0cdcb702f58bdf52fe5fb4f6
Dominic Kramer <DominicDKramer@google.com>
2016-12-21 15:36:08 -0800

    Update `.travis.yml` to run code coverage (#89)
    PR-URL: https://github.com/GoogleCloudPlatform/cloud-errors-nodejs/pull/89

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# fail fast
+set -e
+
 IS_TESTING_LOCALLY=false
 
 function basicSetup {
@@ -15,6 +18,10 @@ function basicTeardown {
   unset NODE_ENV
   rm -rf ./tests/configuration
 }
+
+# Ensure the basicTeardown function is called at the end of this script
+# if the script exits successfully or as a failure
+trap basicTeardown EXIT
 
 function trustedSetup {
   export GCLOUD_PROJECT=`cat ./tests/configuration/stub_project_id.txt`
@@ -67,7 +74,6 @@ function run {
     echo "Running unit suite"
     runUnitSuite
   fi
-  basicTeardown
 }
 
 while getopts "l" opt
